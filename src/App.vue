7d4b2f16659e98e1a9e01ef6922d98e92f422304
Henrik H. Sørensen <hhs19942@gmail.com>
2022-09-28 16:03:09 +0200

    Refinement

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,30 @@
       <h1 class="display-4">Breadboard PC</h1>
     </div>
 
-    <div class="row">
+    <div class="row mb-5">
       <div class="col-lg-10 col-xl-8 mb-3">
         <label for="program-txt" class="form-label">Write your program</label>
-        <textarea v-model="programString" class="form-control" id="program-txt" rows="3"></textarea>
+        <textarea v-model="programString" class="form-control" id="program-txt" rows="16" ></textarea>
       </div>
 
       <div class="col-12">
         <button @click="uploadProgram" type="button" class="btn btn-success btn-lg me-1">Upload</button>
-        <button type="button" class="btn btn-warning btn-lg">Command program</button>
+        <button @click="writeProgram" type="button" class="btn btn-warning btn-lg">Command program</button>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-lg-10 col-xl-8 mb-3">
+        <label for="program-txt" class="form-label">Write a single RAM address</label>
+        <div class="input-group mb-3">
+          <input v-model="data" type="number" class="form-control" placeholder="Value" aria-label="Value">
+          <span class="input-group-text">@</span>
+          <input v-model="address" type="number" min="0" max="15" class="form-control" placeholder="Address" aria-label="Address">
+        </div>
+      </div>
+
+      <div class="col-12">
+        <button @click="writeImmediate" type="button" class="btn btn-success btn-lg me-1">Write</button>
       </div>
     </div>
   </div>
@@ -29,6 +44,8 @@ export default defineComponent({
   data() {
     return {
       programString: '' as string,
+      address: '' as string,
+      data: '' as string
     }
   },
   methods: {
@@ -51,6 +68,30 @@ export default defineComponent({
       };
 
       fetch('https://mqtt.it.fantastiskefroe.dk/publish/program', requestOptions)
+          .then((response) => response.json())
+          .then((data) => console.log(data))
+          .catch(error => console.error('There was an error!', error));
+    },
+    writeProgram(): void {
+      const requestOptions = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({command: "PROGRAM"})
+      };
+
+      fetch('https://mqtt.it.fantastiskefroe.dk/publish/command', requestOptions)
+          .then((response) => response.json())
+          .then((data) => console.log(data))
+          .catch(error => console.error('There was an error!', error));
+    },
+    writeImmediate(): void {
+      const requestOptions = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({command: "WRITE", address: this.address, data: this.data})
+      };
+
+      fetch('https://mqtt.it.fantastiskefroe.dk/publish/command', requestOptions)
           .then((response) => response.json())
           .then((data) => console.log(data))
           .catch(error => console.error('There was an error!', error));
