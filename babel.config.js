49ec69e1e12a14d5403004c2e390cf90489d3a6b
Henrik H. Sørensen <hhs19942@gmail.com>
2022-09-29 17:42:16 +0200

    Add shitty highlighting

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,13 @@
 module.exports = {
     presets: [
         '@vue/cli-plugin-babel/preset'
+    ],
+    'plugins': [
+        ['prismjs', {
+            'languages': ['javascript', 'armasm'],
+            'plugins': ['line-numbers'],
+            'theme': 'tomorrow-night',
+            'css': true
+        }]
     ]
 }
--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,21 @@
     </div>
 
     <div class="row mb-5">
-      <div class="col-6 mb-3">
-        <label for="program-txt" class="form-label">Write your program</label>
-        <textarea v-model="programString" class="form-control" id="program-txt" rows="16"></textarea>
+      <div class="col-6 mb-3 overflow-hidden">
+        <label class="form-label">Write your program</label>
+        <prism-editor class="editor" v-model="programString" :highlight="highlighter" :line-numbers="true"></prism-editor>
       </div>
 
-      <div class="col-4 mb-3">
+      <div class="col-4 mb-3 overflow-hidden">
         <label for="program-txt" class="form-label">Compiled program</label>
-        <p v-if="error" class="text-secondary">
-          {{ error }}
-        </p>
-        <pre v-else>{{ compiledProgram }}</pre>
+        <div v-if="error">
+          <p class="text-secondary">
+            {{ error }}
+          </p>
+        </div>
+        <div v-else>
+          <prism-editor class="editor" v-model="compiledProgram" :highlight="(str) => str" :line-numbers="true" :readonly="true"></prism-editor>
+        </div>
       </div>
 
       <div class="col-12">
@@ -52,27 +56,41 @@
 import '@/assets/style.scss'
 import {defineComponent} from 'vue';
 import * as peggy from 'peggy';
+import {PrismEditor} from 'vue-prism-editor';
+import 'vue-prism-editor/dist/prismeditor.min.css';
+import Prism from 'prismjs';
+import 'prismjs/themes/prism-tomorrow.css';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const grammar = require('@/assets/grammar.peggy?raw');
 
+Prism.manual = true;
+Prism.highlightAll();
+
 export default defineComponent({
   name: 'Breadboard PC programmer',
-  components: {},
+  components: {
+    PrismEditor
+  },
   data() {
     return {
       parser: peggy.generate(grammar),
       programString: '' as string,
-      error: ' ' as string,
+      error: '' as string,
       compiledProgram: '' as string,
       address: '' as string,
       data: '' as string,
     };
   },
   watch: {
-    programString: function (newVal: string) {
+    programString: function (code: string) {
+      if (code.split('\n').length > 16) {
+        this.error = 'Program too long';
+        return;
+      }
+
       let parsed;
       try {
-        parsed = this.parser.parse(newVal);
+        parsed = this.parser.parse(code);
       } catch (e) {
         this.error = e as string;
         return;
@@ -82,6 +100,11 @@ export default defineComponent({
     },
   },
   methods: {
+    highlighter(code: string) {
+      const s = Prism.highlight(code, Prism.languages.armasm, 'armasm');
+      // console.info(s);
+      return s;
+    },
     uploadProgram(): void {
       if (this.error) {
         return;
@@ -134,6 +157,20 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
+.editor {
+  background: #2d2d2d;
+  color: #ccc;
+
+  font-family: Fira code, Fira Mono, Consolas, Menlo, Courier, monospace;
+  font-size: 14px;
+  line-height: 1.5;
+  padding: 5px;
+}
+
+.prism-editor__textarea:focus {
+  outline: none;
+}
+
 pre {
   font-size: 1rem;
 }
