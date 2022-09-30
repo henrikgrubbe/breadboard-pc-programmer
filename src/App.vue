7eb5151cf8b65088236e206b007eec673a3e6825
Henrik H. Sørensen <hhs19942@gmail.com>
2022-09-30 13:28:44 +0200

    Bring back single line write

--- src/App.vue
+++ src/App.vue
@@ -43,24 +43,24 @@
       </div>
     </div>
 
-    <!--    <div class="row">-->
-    <!--      <div class="col-lg-10 col-xl-8 mb-3">-->
-    <!--        <label for="program-txt" class="form-label">Write a single RAM address</label>-->
-    <!--        <div class="input-group mb-3">-->
-    <!--          <input v-model="data" type="number" min="0" max="255" class="form-control" placeholder="Value"-->
-    <!--                 aria-label="Value">-->
-    <!--          <span class="input-group-text">@</span>-->
-    <!--          <input v-model="address" type="number" min="0" max="15" class="form-control" placeholder="Address"-->
-    <!--                 aria-label="Address">-->
-    <!--        </div>-->
-    <!--      </div>-->
+    <div class="row">
+      <div class="col-lg-10 col-xl-8 mb-3">
+        <label for="program-txt" class="form-label">Write a single RAM address</label>
+        <div class="input-group mb-3">
+          <input v-model="data" type="number" min="0" max="255" class="form-control" placeholder="Value"
+                 aria-label="Value">
+          <span class="input-group-text">@</span>
+          <input v-model="address" type="number" min="0" max="15" class="form-control" placeholder="Address"
+                 aria-label="Address">
+        </div>
+      </div>
 
-    <!--      <div class="col-12">-->
-    <!--        <button @click="writeImmediate" :disabled="data === '' || address === ''" type="button"-->
-    <!--                class="btn btn-success btn-lg me-1">Write-->
-    <!--        </button>-->
-    <!--      </div>-->
-    <!--    </div>-->
+      <div class="col-12">
+        <button @click="writeImmediate" :disabled="data === '' || address === ''" type="button"
+                class="btn btn-success btn-lg me-1">Write
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
